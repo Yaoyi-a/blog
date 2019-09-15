3635db297bfded8a297dc3785ab317820971b3b2
Yaoyi <13110235070@163.com>
2019-09-15 17:48:21 +0800

    config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  title: "姚欢的个人博客",
+  title: "yaoyi's blog(() => {
+    
+  }, interval);",
   description: "姚欢的个人博客",
   head: [
     ['link', { rel: 'icon', href: '/logo.png' }]
@@ -7,7 +9,7 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '我的主页', link: '/' },
-      { text: '笔迹心得', link: '/note/' },
+      { text: '笔ji心得', link: '/note/' },
       { text: '个人空间', link: 'https://user.qzone.qq.com/158737832/main' },
     ],
     sidebar: 'auto'
